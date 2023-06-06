6e5ce1fc537a948c4b6765fb419d909028c42b33
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-06-06 02:57:58 +0000

    Added the main icon of the website.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <!-- The main page of the website -->
 <script lang="ts">
   import ThemeToggler from "$lib/components/ThemeToggler.svelte";
+  import MainIcon from "$lib/components/MainIcon.svelte";
 </script>
 
 <!-- The HTML for the main page -->
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<ThemeToggler />
+<MainIcon />
