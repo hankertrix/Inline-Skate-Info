030d949c53b0bb9c8fcc7e22bd4fe06648e1eeb7
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-06-29 16:29:06 +0000

    Made the link to the license open in a new tab

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -95,7 +95,7 @@
 
 <!-- The main HTML for the footer -->
 <footer class="footer">
-  <a href="https://codeberg.org/Hanker/Inline-Skate-Info/src/branch/main/LICENSE.txt" class="copyleft" title="Copyright notice and license" rel="noopener noreferrer">
+  <a class="copyleft" href="https://codeberg.org/Hanker/Inline-Skate-Info/src/branch/main/LICENSE.txt" target="_blank" title="Copyright notice and license" rel="noopener noreferrer">
     <div class="text">
       Copyleft&nbsp;<span class="symbol">©</span>&#32;2023 Hankertrix.
       <div class="rights-notice">All Wrongs Reserved.</div>
