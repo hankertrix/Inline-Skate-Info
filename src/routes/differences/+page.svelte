1f930526a969396d926199943ff51105f851b439
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-01-05 01:18:32 +0800

    Make the grid have 4 items instead of 3

--- src/routes/differences/+page.svelte
+++ src/routes/differences/+page.svelte
@@ -65,7 +65,7 @@
 
   .links-grid {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     justify-content: center;
     align-items: center;
     gap: 2em;
