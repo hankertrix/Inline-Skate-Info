baa53fa9cb2e1e007511b61d4a8219a73bd46624
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-01-11 20:42:16 +0800

    Fixed up the formatting in the bot-utils.ts file

--- telegram-bot/bot-utils.ts
+++ telegram-bot/bot-utils.ts
@@ -9,9 +9,19 @@ import type {
 } from "telegraf/types";
 import type { OptionalPropertiesOf } from "./types";
 import { Scenes, Markup } from "telegraf";
-import { MAX_CHARACTERS, CACHE_TIME, SPACING, OPERATION_CANCELLED_MSG, EXIT_MESSAGE, MESSAGE_ENTITY_LIMIT } from "../src/lib/constants";
 import * as utils from "./utils";
-import { ENABLE_DELETING_COMMAND_MESSAGES, ENABLE_ADMIN_CHECK } from "../config";
+import {
+  MAX_CHARACTERS,
+  CACHE_TIME,
+  SPACING,
+  OPERATION_CANCELLED_MSG,
+  EXIT_MESSAGE,
+  MESSAGE_ENTITY_LIMIT
+} from "../src/lib/constants";
+import {
+  ENABLE_DELETING_COMMAND_MESSAGES,
+  ENABLE_ADMIN_CHECK
+} from "../config";
 
 // The regular expression to get the message entities (currently HTML tags)
 const messageEntityRegex = /<.+?>/g;
