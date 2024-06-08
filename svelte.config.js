7cd74c97231057840879c8d0d6edf22eafa2eee8
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-06-08 18:21:36 +0800

    Fixed the hashes

--- svelte.config.js
+++ svelte.config.js
@@ -16,11 +16,15 @@ const config = {
         "frame-src": ["https://www.youtube-nocookie.com"],
         "script-src": [
           "self",
-          "sha512-4515d140a78b1b2bcbba2b52c9513c64a63cb02a4f81343f3a6c19855003052a039fc834919d3f1a455affd0114985302a97e24d4a90af6fbdf17063fcdb860e",
         ],
         "style-src": [
           "self",
-          "sha512-2a551a0bfea69a81b2d4fde02ec5831f89b552870e515c4c09121b4eed4830dd8f4819083016b55bbd7396dc89c99cfae989871fefa3f6de609bb79d2012b2d4"
+
+          // Main CSS style for the entire page
+          "sha256-EHFod+1+yJWH4JfktKYKAH7HTu+l8xj88fKU0IUyX/U=",
+
+          // Styles for the light and dark theme toggle
+          "sha256-ysd0CFQu/mXJ7dH4GWPr2Y3mx7J2r75LcZQVHMH+5Ss="
         ]
       }
     }
