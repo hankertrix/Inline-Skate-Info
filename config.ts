bdb64ba65326a00bb144fc7b4aa34f76173bf751
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-09-14 16:34:09 +0800

    Reformat the file

--- config.ts
+++ config.ts
@@ -4,8 +4,10 @@
 // Should be turned off after setting the webhook in production
 export const ENABLE_SETTING_WEBHOOK = false;
 
-// The setting to enable the check for admins in group chats for the poll and training message commands
+// The setting to enable the check for admins in group chats
+// for the poll and training message commands
 export const ENABLE_ADMIN_CHECK = false;
 
-// The setting to enable deleting command messages in group chats for the poll and training message commands
+// The setting to enable deleting command messages in group chats
+// for the poll and training message commands
 export const ENABLE_DELETING_COMMAND_MESSAGES = true;
