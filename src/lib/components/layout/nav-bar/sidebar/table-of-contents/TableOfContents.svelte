c78804e929975bb9787668153ae00e158e29033d
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-14 00:34:52 +0800

    Reformatted the file and fix a lint by eslint

--- src/lib/components/layout/nav-bar/sidebar/table-of-contents/TableOfContents.svelte
+++ src/lib/components/layout/nav-bar/sidebar/table-of-contents/TableOfContents.svelte
@@ -1,6 +1,5 @@
 <!-- The component to generate the table of contents for the side bar -->
 <script lang="ts">
-
   import type { TableOfContents } from "$lib/types";
   import { page } from "$app/stores";
   import { browser } from "$app/environment";
@@ -8,19 +7,19 @@
 
   // The variable to store the list of headings on the page
   // Eslint is somehow not recognising NodeListOf
-  // as a defined type, hence the line below
+  // defined type, hence the line below
   // eslint-disable-next-line  no-undef
   let headings: NodeListOf<Element>;
 
   // The variable to store the table of contents object
   let tableOfContents: TableOfContents = new Map();
 
-  
   // Function to get the table of contents
   // Eslint is somehow not recognising NodeListOf
   // as a defined type, hence the line below
   // eslint-disable-next-line  no-undef
   function getTableOfContents(headings: NodeListOf<Element>) {
+    //
 
     // The object representing the table of contents
     const tableOfContents = new Map();
@@ -36,12 +35,14 @@
 
     // Iterates over all the headings in the document
     for (const heading of headings) {
+      //
 
       // The level of the current heading
       const currentHeadingLevel = parseInt(heading.tagName.substring(1).trim());
 
       // If the heading is the first heading
       if (firstHeadingLevel < 0) {
+        //
 
         // Sets the first heading level to the level of the first heading
         firstHeadingLevel = currentHeadingLevel;
@@ -52,45 +53,64 @@
         // Adds the heading to the table of contents
         tableOfContents.set(heading.textContent, {
           id: heading.id,
-          children: new Map()
+          children: new Map(),
         });
 
-        // Adds the reference to the current heading to the list of previous headings, at the position of its level
-        previousHeadings[currentHeadingLevel] = tableOfContents.get(heading.textContent).children;
+        // Adds the reference to the current heading
+        // to the list of previous headings, at the position of its level
+        previousHeadings[currentHeadingLevel] = tableOfContents.get(
+          heading.textContent
+        ).children;
 
-        // Adds the reference to the entire table of contents to the list of previous headings, one level below the current heading level
+        // Adds the reference to the entire table of contents to the
+        // list of previous headings, one level below the current heading level
         previousHeadings[currentHeadingLevel - 1] = tableOfContents;
 
         // Continues the loop
         continue;
       }
 
-        // Otherwise, if the current heading level is lower than that of the first heading level, throw an error
+      // Otherwise, if the current heading level is lower than that
+      // of the first heading level, throw an error
       else if (currentHeadingLevel < firstHeadingLevel) {
-        throw Error(`Failed to generate table of contents as the heading "${heading.textContent}" has a smaller heading level (${`h${currentHeadingLevel}`}) than the first heading level (${`h${firstHeadingLevel}`}).`);
+        throw Error(
+          `Failed to generate table of contents as the heading "${
+            heading.textContent
+          }" has a smaller heading level (` +
+            `h${currentHeadingLevel}` +
+            `) than the first heading level (${`h${firstHeadingLevel}`}).`
+        );
       }
 
       // Initialise the previous heading variable
       let previousHeading = null;
 
-      // If the current heading level is greater than the previous heading level
+      // If the current heading level is greater than the
+      // previous heading level
       if (currentHeadingLevel > previousHeadingLevel) {
+        //
 
         // Gets the previous heading from the list of previous headings
         previousHeading = previousHeadings[previousHeadingLevel];
       }
 
-        // If the current heading level is the same or less than that of the previous heading
+      // If the current heading level is the same or
+      // less than that of the previous heading
       else if (currentHeadingLevel <= previousHeadingLevel) {
+        //
 
         // Initialise the level variable to the current heading level minus 1
         let level = currentHeadingLevel - 1;
 
-        // Gets the previous heading has a level lower than the current heading level
-        // Iterates backwards while the level is greater or equal to zero and the previous heading exists
+        // Gets the previous heading has a level lower
+        // than the current heading level.
+        // Iterates backwards while the level is greater or equal to zero
+        // and the previous heading exists.
         while (level >= 0 && previousHeading == null) {
+          //
 
-          // Gets the previous heading reference from the list of previous headings
+          // Gets the previous heading reference from the list of
+          // previous headings
           previousHeading = previousHeadings[level];
 
           // Decrease the level variable by 1
@@ -101,11 +121,14 @@
       // Adds the current heading to the previous heading
       previousHeading.set(heading.textContent, {
         id: heading.id,
-        children: new Map()
+        children: new Map(),
       });
 
-      // Adds the current heading to the list of previous headings at its current level
-      previousHeadings[currentHeadingLevel] = previousHeading.get(heading.textContent).children;
+      // Adds the current heading to
+      // the list of previous headings at its current level
+      previousHeadings[currentHeadingLevel] = previousHeading.get(
+        heading.textContent
+      ).children;
 
       // Set the previous heading level to the current heading level
       previousHeadingLevel = currentHeadingLevel;
@@ -114,32 +137,34 @@
     // Returns the table of contents
     return tableOfContents;
   }
-  
 
   // Reactive block
   $: {
+    //
 
     // This is just to force the component to re-render when the route changes
-    $page.url;
+    // eslint-disable-next-line  @typescript-eslint/no-unused-vars
+    let throwaway = $page.url;
 
     // If the environment is the browser
     if (browser) {
-      
+      //
+
       // Gets all the headings in the document
       headings = document.querySelectorAll("h2, h3, h4, h5, h6");
 
       // Gets the table of contents from the page
       tableOfContents = tableOfContents.set("Table Of Contents", {
         id: "",
-        children: getTableOfContents(headings)
+        children: getTableOfContents(headings),
       });
     }
   }
-  
 </script>
 
 <!-- The HTML for the table of contents -->
-<!-- Displays the table of contents only when the number of headings on the page is greater than 2 -->
+<!-- Displays the table of contents only when the number
+of headings on the page is greater than 2 -->
 {#if headings && headings.length > 1}
   <TableOfContentsCollapsibleMenu {tableOfContents} />
 {/if}
