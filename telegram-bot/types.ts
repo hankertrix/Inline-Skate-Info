d759e2282d7b9cc2a11a4bc999fa13cdb0025df6
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-12-29 13:08:22 +0800

    Reformat the file

--- telegram-bot/types.ts
+++ telegram-bot/types.ts
@@ -5,68 +5,62 @@ import type {
   CallbackQuery,
   Message,
   InlineKeyboardButton,
-  InlineKeyboardMarkup
+  InlineKeyboardMarkup,
 } from "telegraf/types";
 
 // The dictionary type
 export type Dict<T> = {
-  [key: string | number]: T | Dict<T>
+  [key: string | number]: T | Dict<T>;
 };
-
 
 // The type representing a dictionary that can be reversed
 export type ReversibleDict = {
-  [key: string | symbol | number]: string | symbol | number
+  [key: string | symbol | number]: string | symbol | number;
 };
-
 
 // The type representing object values
 export type ObjectValues<T> = T[keyof T];
 
-
 // The type representing the optional properties of a given type
 export type OptionalPropertiesOf<T extends object> = {
-  [K in keyof T as T extends Record<K, T[K]> ? never : K]: T[K]
+  [K in keyof T as T extends Record<K, T[K]> ? never : K]: T[K];
 };
-
 
 // The type representing a callback query
 export type CbQuery = CallbackQuery & {
-  data: string,
+  data: string;
   message: Message.TextMessage & {
     reply_markup: {
-      inline_keyboard: InlineKeyboardButton[][]
-    }
-  }
+      inline_keyboard: InlineKeyboardButton[][];
+    };
+  };
 };
 
-
 // The type of the inline keyboard generator function
-export type InlineKeyboardGenerator =
-  (pollOption: string[]) => Types.Markup<InlineKeyboardMarkup>;
-
+export type InlineKeyboardGenerator = (
+  pollOption: string[]
+) => Types.Markup<InlineKeyboardMarkup>;
 
 // The type representing a product
 export type Product = {
-  price: string,
-  link: string
+  price: string;
+  link: string;
+  description?: string;
 };
-
 
 // The type representing a place
 export type Place = {
-  address?: string,
-  hours?: string,
-  price?: string,
-  phone?: string,
-  email?: string,
-  website?: string,
-  mapLink?: string,
-  description?: string
+  address?: string;
+  hours?: string;
+  price?: string;
+  phone?: string;
+  email?: string;
+  website?: string;
+  mapLink?: string;
+  description?: string;
 };
-
 
 // The type representing a date mapping
 export type DateMapping = {
-  [day: number]: Date
+  [day: number]: Date;
 };
