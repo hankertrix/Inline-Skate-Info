c1f812864a4d7059025c3fd91176812c1ed0fbf6
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-01-20 17:59:56 +0800

    Fixed the eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,48 @@
 import prettier from "eslint-config-prettier";
 import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
+import { fileURLToPath } from "node:url";
 import ts from "typescript-eslint";
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 export default ts.config(
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs["flat/recommended"],
-  prettier,
-  ...svelte.configs["flat/prettier"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-  },
-  {
-    files: ["**/*.svelte"],
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs["flat/recommended"],
+	prettier,
+	...svelte.configs["flat/prettier"],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	{
+		files: ["**/*.svelte"],
 
-    languageOptions: {
-      parserOptions: {
-        parser: ts.parser,
-      },
-    },
-  },
-  {
-    ignores: [
-      "build/",
-      ".svelte-kit/",
-      "dist/",
-      "**/.DS_Store",
-      "**/node_modules",
-      ".netlify",
-      "**/.env",
-      "**/.env.*",
-      "!**/.env.example",
-      "static/",
-    ],
-  }
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser,
+			},
+		},
+	},
+	{
+		ignores: [
+			"build/",
+			".svelte-kit/",
+			"dist/",
+			"**/.DS_Store",
+			"**/node_modules",
+			".netlify",
+			"**/.env",
+			"**/.env.*",
+			"!**/.env.example",
+			"static/",
+		],
+	}
 );
