6c12f96e00faa715d9814cd27f14a22bd841176c
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-07-30 14:41:18 +0800

    Reformat the scroll up button file

--- src/lib/components/layout/ScrollUpButton.svelte
+++ src/lib/components/layout/ScrollUpButton.svelte
@@ -1,6 +1,5 @@
 <!-- The button to scroll up to the top of a page -->
 <script lang="ts">
-
   import { onMount } from "svelte";
 
   // The scroll height threshold in pixels
@@ -13,19 +12,26 @@
   // The variable to store the previous scroll height
   let previousScrollHeight: number = 0;
 
-
   // The function to make the scroll up button appear and disappear
   function handleScroll() {
+    //
 
-    // If the scroll up button is null, or if the window isn't defined, immediately exit the function
+    // If the scroll up button is null,
+    // or if the window isn't defined, immediately exit the function
     if (scrollUpButton == null || typeof window === "undefined") return;
 
     // Gets the current scroll height
     const currentScrollHeight = window.scrollY;
 
-    // If the current scroll height is greater than the scroll height threshold
-    // And if the current scroll height is less than the previous scroll height (this means the user is scrolling upwards)
-    if (currentScrollHeight > SCROLL_HEIGHT_THRESHOLD && currentScrollHeight < previousScrollHeight) {
+    // If the current scroll height is greater than the
+    // scroll height threshold and if the current scroll height
+    // is less than the previous scroll height,
+    // which means the user is scrolling upwards
+    if (
+      currentScrollHeight > SCROLL_HEIGHT_THRESHOLD &&
+      currentScrollHeight < previousScrollHeight
+    ) {
+      //
 
       // Makes the scroll up button visible
       scrollUpButton!.style.visibility = "visible";
@@ -34,6 +40,7 @@
 
     // Otherwise
     else {
+      //
 
       // Makes the scroll up button invisible
       scrollUpButton!.style.opacity = "0";
@@ -44,23 +51,41 @@
     previousScrollHeight = currentScrollHeight;
   }
 
-
   // The function to run when the component is mounted
   onMount(() => {
+    //
 
-    // Adds the event listener for the scroll event to the docunment
+    // Adds the event listener for the scroll event to the document
     document.addEventListener("scroll", handleScroll);
 
     // Gets the scroll up button
     scrollUpButton = document.getElementById("scroll-up-button");
-
   });
-
 </script>
+
+<!-- The HTML for the scroll up button -->
+<nav id="scroll-up-button" title="Scroll to the top of the page">
+  <a href="#top-of-the-page">
+    <!---->
+
+    <!-- The SVG for the scroll up button -->
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 320 512"
+    >
+      <path
+        fill="currentColor"
+        d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+      ></path>
+    </svg>
+  </a>
+</nav>
 
 <!-- The styles for the scroll up button -->
 <style>
-
   nav {
     --size: 2.5rem;
 
@@ -73,7 +98,9 @@
     width: var(--size);
     height: var(--size);
     z-index: 3;
-    transition: opacity var(--animation-timing), visibility var(--animation-timing);
+    transition:
+      opacity var(--animation-timing),
+      visibility var(--animation-timing);
   }
 
   a {
@@ -91,17 +118,4 @@
     height: 100%;
     translate: 0 -2px;
   }
-
 </style>
-
-<!-- The HTML for the scroll up button -->
-<nav id="scroll-up-button" title="Scroll to the top of the page">
-
-  <a href="#top-of-the-page">
-
-    <!-- The SVG for the scroll up button -->
-    <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-      <path fill="currentColor" d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"></path>
-    </svg>
-  </a>
-</nav>
