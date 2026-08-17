3313000acf3227d33cf155ce28d1e55f425fe267
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2026-08-17 12:28:13 +0200

    Actually properly fix the CSP

--- svelte.config.js
+++ svelte.config.js
@@ -26,27 +26,21 @@ const config = {
 
 					// For Pagefind to work
 					"wasm-unsafe-eval",
-
-					// The script in app.html to stop the page from flashing
-					"sha256-d2pQ/3PfzMyNVJrM8tHklReh2FBEi4YJneAGxVKHrtM=",
 				],
 
 				"style-src": [
 					"self",
 
-					// Main CSS style for the entire page
-					"sha256-DVjIOpTXG5xePW6r2ugbRkW/OhSOWeU36LRR6SqlgQo=",
-
-					// Styles for the website icon
-					"sha256-GQHiEoT/Yyw7i/CQpbsUjU+CrrV6C6C7U5iq+niceqw=",
-
-					// Styles for the Pagefind highlight script
-					"sha256-ZCoB1kba9ZTfELA9P5NVsXbrXfcqOSTrLTbcMfUVY4E=",
-
-					// This is needed for the style below it to work
+					// This is needed for the styles below it to work
 					"unsafe-hashes",
 
-					// No idea what style this is for, but it's required
+					// The main CSS style for the entire page
+					"sha256-6a9+Lds8WKfdxKRBnSKAnPOnS463+PcAPc/b7NWjtoY=",
+
+					// The styles for the main icon of the site
+					"sha256-GQHiEoT/Yyw7i/CQpbsUjU+CrrV6C6C7U5iq+niceqw=",
+
+					// The styles for Svelte
 					"sha256-S8qMpvofolR8Mpjy4kQvEm7m1q8clzU4dfDH0AmvZjo=",
 				],
 			},
